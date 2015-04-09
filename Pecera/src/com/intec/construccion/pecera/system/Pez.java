a2075a37c5955eceba8b63e3be0d0e16b62f5d2b
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2015-04-09 03:19:31 -0400

    Pez se ve en pantalla y se mueve aleatoriamente

--- Pecera/src/com/intec/construccion/pecera/system/Pez.java
+++ Pecera/src/com/intec/construccion/pecera/system/Pez.java
@@ -1,139 +1,135 @@
 package com.intec.construccion.pecera.system;
 
-import java.io.IOException;
-import java.util.Arrays;
+import java.awt.Point;
 import java.util.Random;
 
-public class Pez extends Thread{
-	private char sexo;
-	
-	
-	private Integer pasos = 20;
-	Random rand = new Random();
-	int number;
-	
-	String[][] matrix = new String[][]{
-			{" 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "},
-			{" 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "},
-			{" 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "},
-			{" 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "},
-			{" 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "},
-			{" 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "},
-			{" 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "},
-			{" 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "},
-			{" 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "},
-			{" 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "," 0 "}
-	};
-	
-	public Pez(char sexo){
-		this.setSexo(sexo);
-	}
+import javax.swing.JLabel;
 
+public class Pez extends Thread{
+
+	private char sexo;
+	private JLabel label;
+	private int limite;
+	private Point puntoLabel;
+	
+	public Pez(char sexo, JLabel label, int limite, Point punto){
+		this.setSexo(sexo);
+		this.setLabel(label);
+		this.setLimite(limite);
+		this.puntoLabel = punto;
+	}
+	
 	@Override
 	public void run(){
-		int x = randInt(0,9);
-		int y = randInt(0,9);
+		int X = puntoLabel.x;
+		int Y = puntoLabel.y;
 		
-		matrix[x][y] = "><>";
-		
-		showMatrix();
-		
-		moveOnMatrix(x,y);
+		for(int i = 0; i < limite; i++){
+			try {
+				
+				//obtiene el punto presente del label
+				
+				
+				Point punto = moveOnMatrix(X,Y);
+				
+				X = punto.x;
+				Y = punto.y;
+				
+				label.setLocation(X,Y);
+				
+				Thread.sleep(70);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		
 		yield();
 	}
 	
-	private void moveOnMatrix(int x, int y){
+	//genera un punto aleatorio al rededor de las coordenadas de parametro 
+	private Point moveOnMatrix(int x, int y){
+		Random rand = new Random();
 		
-		for(int i = 0; i < pasos; i++){
-			number = rand.nextInt(5);
+		Point punto = null;
+		
+		int number = rand.nextInt(8);
+		
+		switch(number){
+		case 0:	//izquierda
 			
-			if(number == 0){
-				clearConsole();
-				//0 = izquierda
-				if(y+1 != 10){
-					matrix[x][y] = " 0 ";
-					matrix[x][y+1] = "><>";
-					y++;
-				}else{
-					matrix[x][y] = " 0 ";
-					matrix[x][y-1] = "><>";
-					y--;
-				}
-				showMatrix();
-			}else if(number == 1){
-				clearConsole();
-				//1 = abajo
-				if(x+1 != 10){
-					matrix[x][y] = " 0 ";
-					matrix[x+1][y] = "><>";
-					x++;
-				}else{
-					matrix[x][y] = " 0 ";
-					matrix[x-1][y] = "><>";
-					x--;
-				}
-				showMatrix();
-			}else if(number == 2){
-				clearConsole();
-				//2 = derecha
-				if(y-1 != 0){
-					matrix[x][y] = " 0 ";
-					matrix[x][y-1] = "><>";
-					y--;
-				}else{
-					matrix[x][y] = " 0 ";
-					matrix[x][y+1] = "><>";
-					y++;
-				}
-				showMatrix();
-			}else if(number == 3){
-				clearConsole();
-				//3 = arriba
-				if(x-1 != 0){
-					matrix[x][y] = " 0 ";
-					matrix[x-1][y] = "><>";
-					x--;
-				}else{
-					matrix[x][y] = " 0 ";
-					matrix[x+1][y] = "><>";
-					x++;
-				}
-				showMatrix();
-			}
+			if(x-1 > 0)
+				punto = new Point(x-1,y);
 			
+			break;
+		case 1:	//derecha
+			
+			if(x+1 < 1300)
+				punto = new Point(x+1,y);
+			
+			break;
+		case 2:	//arriba
+			
+			if(y-1 > 0)
+				punto = new Point(x,y-1);
+			
+			break;
+		case 3:	//abajo
+			
+			if(y+1 < 700)
+				punto = new Point(x,y+1);
+			
+			break;
+		case 4:	//superior izquierda
+			
+			if(x-1 > 0 && y-1 >0)
+				punto = new Point(x-1,y-1);
+			
+			break;
+		case 5:	//superior derecha
+			
+			if(x+1 < 1300 && y-1 > 0)
+				punto = new Point(x+1,y-1);
+			
+			break;
+		case 6:	//inferior izquierda
+			
+			if(x-1 > 0 && y+1 < 700)
+				punto = new Point(x-1,y+1);
+			
+			break;
+		case 7:	//inferior derecha
+			
+			if(x+1 < 1300 && y+1 < 700)
+				punto = new Point(x+1,y+1);
+				
+			break;
 		}
+			
+		return punto;
 	}
 
-	private void clearConsole(){
-		for(int clear = 0; clear < 10; clear++)
-		  {
-		     System.out.println("\n") ;
-		  }
-	}
-	
-	public void showMatrix(){
-		for(int i = 0; i < 10; i++){
-			for(int j = 0; j < 10; j++){
-				System.out.print(matrix[i][j]);
-			}
-			System.out.print("\n");
-		}
-	}
-	
-	private int randInt(int min, int max) {
 
-	    int randomNum = rand.nextInt((max - min) + 1) + min;
-
-	    return randomNum;
-	}
-	
 	public char getSexo() {
 		return sexo;
 	}
 
-	public void setSexo(char sexo2) {
-		this.sexo = sexo2;
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
 	}
 	
+	public JLabel getLabel() {
+		return label;
+	}
+
+	public void setLabel(JLabel label) {
+		this.label = label;
+	}
+
+	public int getLimite() {
+		return limite;
+	}
+
+	public void setLimite(int limite) {
+		this.limite = limite;
+	}
 }
